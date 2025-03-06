6ace116db23e0afdbff1ab2611979090b983a5b7
John095 <johnouma095@gmail.com>
2025-03-06 15:33:00 +0300

    Fix 5

--- src/components/user-profile/UserMetaCard.tsx
+++ src/components/user-profile/UserMetaCard.tsx
@@ -78,7 +78,7 @@ export default function UserMetaCard() {
                 </svg>
               </a>
 
-              <a href="https://www.linkedin.com/company/pimjo" target="_blank"
+              <a href="https://www.linkedin.com/" target="_blank"
         rel="noreferrer" className="flex h-11 w-11 items-center justify-center gap-2 rounded-full border border-gray-300 bg-white text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
                 <svg
                   className="fill-current"
